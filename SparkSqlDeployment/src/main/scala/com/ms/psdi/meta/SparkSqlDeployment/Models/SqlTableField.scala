d7e02b389c4bad7e92550f6a56742adde0f9613c
Rajesh Rao <rajrao@microsoft.com>
2021-01-05 21:46:28 +0530

    Removing Unnecesssary module for github deployment

--- SparkSqlDeployment/src/main/scala/com/ms/psdi/meta/SparkSqlDeployment/Models/SqlTableField.scala
+++
@@ -1,5 +0,0 @@
-package com.ms.psdi.meta.SparkSqlDeployment.Models
-
-import net.liftweb.json.JsonAST.JValue
-
-case class SqlTableField(name: String, datatype: String, nullable: Boolean, metadata: Map[String, JValue])
